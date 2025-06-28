e0b8d2b006b1923fac3f6ed9f877648f1795cf25
Nimadepramestidewi5 <pramestidewi2005@gmail.com>
2025-06-28 14:54:05 +0800

    Add files via upload

---
+++ OOP-UAS-C-2301010322/src/oop/uas/c/pkg2301010322/OOPUASC2301010322.java
@@ -0,0 +1,17 @@
+package oop.uas.c.pkg2301010322;
+
+/**
+ *
+ * @author Ni Made Pramesti Dewi
+ * 2301010322
+ */
+public class OOPUASC2301010322 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
